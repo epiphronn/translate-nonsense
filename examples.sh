48a0aafdc9a28f5d14624ebf34142fec7522a175
patrickjf44@gmail.com <041200pjf>
2018-06-21 14:55:45 -0500

    update examples

--- examples.sh
+++ examples.sh
@@ -4,11 +4,10 @@
 # It takes a kind of stupid amount of times to run.
 # Provide the script an argument which is the amt of times to run through translation.
 
-echo "Example text undergoing 5 runs of the script:"
+echo "Example text undergoing ${1-5} runs of the script:"
 TEXT="This is the text that will be translated multiple times, lol!"
 echo $TEXT
-for (( i = 1; i <= 5; i++ )); do
+for i in $(seq 1 5); do
   echo -n "$i. "
-  py make_nonsense.py "$TEXT" 5 en
+  py make_nonsense.py "$TEXT" ${1-5} en
 done
-#!/bin/sh
